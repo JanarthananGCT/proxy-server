edc07047fe422c0ce76abc867fc4f683cfb34bf5
Janarthanan S <89519757+JanarthananGCT@users.noreply.github.com>
2024-12-27 09:23:47 +0530

    Add files via upload

---
+++ api/server.js
@@ -0,0 +1,8 @@
+export default (req, res) => {
+    res.json({
+      id: 1,
+      displayName: "Surveysparrow",
+      avatarUrl: "https://appnest-app.salesparrow.com/SurveyMigrationTest-4119/version_1.0/icon/Color.png"
+    });
+  };
+  
