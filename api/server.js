1d5464102a44000957c682b436e7697c59a4b7b5
Janarthanan S <janarthanans@SSLAP0299.local>
2025-01-10 10:54:05 +0530

    Fronted Condition Fixes

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
-export default (req, res) => {
-    console.log(req?.headers)
-     // console.log(req?.body)
-    res.json({
-      id: 1,
-      displayName: "Surveysparrow",
-      avatarUrl: "https://appnest-app.salesparrow.com/SurveyMigrationTest-4119/version_1.0/icon/Color.png"
+import axios from "axios";
+export default async (req, res) => {
+    let response = await axios.get("https://api.surveysparrow.com/v3/users", {
+        headers: {
+            Authorization: req?.headers?.authorization
+        }
     });
+    response.data.token = req?.headers?.authorization
+    console.log(response?.data)
+    res.json(response)
   };
   
