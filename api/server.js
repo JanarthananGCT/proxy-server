3510712f9581eacc2983ce97cf653b5df49f79c5
Janarthanan S <89519757+JanarthananGCT@users.noreply.github.com>
2025-02-06 12:55:36 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,16 +1,19 @@
 export default async (req, res) => {
   try {
-    const apiResponse = await fetch("https://api.surveysparrow.com/v3/users", {
-      method: "GET",
-      headers: {
-        Authorization: req?.headers?.authorization || "",
-      },
-    });
-    const data = await apiResponse.json();
-    data.token = req?.headers?.authorization;
-    console.log(req.headers, "headers")
-    console.log(req.body, "body")
-    console.log(data, "data")
+    // const apiResponse = await fetch("https://api.surveysparrow.com/v3/users", {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: req?.headers?.authorization || "",
+    //   },
+    // });
+    // const data = await apiResponse.json();
+    // data.token = req?.headers?.authorization;
+    // console.log(req.headers, "headers")
+    // console.log(req.body, "body")
+    // console.log(data, "data")
+    const data = {
+      name : "Jana"
+    }
 
     res.status(200).json(data);
   } catch (error) {
